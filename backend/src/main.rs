afaf7a1a3ef1ed8802d99b7923e0dc16cf7aaffe
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-14 13:04:37 +0200

    building api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -24,3 +24,58 @@ const OK_RESPONSE: &str =
     "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT Found\r\n\r\n";
 const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+
+//main function
+
+fn main() {
+    //set database
+    if let Err(_) = set_database() {
+        println!("Error setting databse");
+        return;
+    }
+
+    // start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handle_client(stream),
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
+}
+
+//db setup
+
+fn set_database() -> Result<(), PostgressError> {
+    let mut client = Client::connect(DB_URL, NotTls)?;
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )
+        ",
+    )?;
+    Ok(())
+}
+
+// get id from request url
+fn get_id(request: &str) -> &str {
+    request
+        .split("/")
+        .nth(4)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default()
+}
+
+//deserialze user from request body with id
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
